ccc251154bac93417e592799e0ccea91b71212e4
Gabib <your-github-email@example.com>
2026-01-30 15:47:53 -0300

    cm

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ wget https://raw.githubusercontent.com/codemanticism/vzilla/refs/heads/master/vz
 chmod +x vzilla-c
 chmod +x vzillash.sh
 chmod +x vzilla
-echo 'mycmd() {
+echo 'vzilla_func() {
     cd ~/.local/bin
     source vzilla $1 $2
     cd -
 }
-alias vzilla="mycmd"' >> ~/.bashrc
+alias vzilla="vzilla_func"' >> ~/.bashrc
 cd -
 echo "Exit the session and then re-enter."
